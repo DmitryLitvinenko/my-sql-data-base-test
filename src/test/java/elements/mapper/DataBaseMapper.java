11f06d8ab19981681cfed697fcfbd764dc48c223
dmitryl <dmitrijs.litvinenko@neotech.lv>
2020-02-07 17:23:36 +0200

    delete: redundant class

--- src/test/java/elements/mapper/DataBaseMapper.java
+++
@@ -1,9 +0,0 @@
-package elements.mapper;
-
-import org.apache.ibatis.annotations.Select;
-
-public interface DataBaseMapper {
-
-    @Select("CREATE DATABASE #{name}")
-    void createDataBase(String name);
-}
